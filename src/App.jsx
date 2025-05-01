5292c073324d070daccfd0ca67fcad9eb91965ba
Charles Johnston <charles.ward.johnston@gmail.com>
2025-05-01 18:44:15 -0400

    fix issue where refresh token was not saved (becuase now the frontend and backend are different domains)

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ export default function App() {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
+      credentials: "include",
     });
 
     const data = await res.json();
