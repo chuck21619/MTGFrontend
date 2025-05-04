5e24a6d6f2d84c098fac4b1b65ec7b18c1dbc633
Charles Johnston <charles.ward.johnston@gmail.com>
2025-05-04 14:02:41 -0400

    removing all testing

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host:'0.0.0.0',
-    allowedHosts: ['frontend', 'localhost'],
     proxy: {
-      '/api': 'http://backend:8080',
+      '/api': 'http://localhost:8080', // Local backend address
     },
   },
 })
